2ec49b937006114e3144dfd8f399665bb9cb71b8
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2024-11-07 13:02:21 +0100

    cleanup + const
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- modules/EvAPI/EvAPI.cpp
+++ modules/EvAPI/EvAPI.cpp
@@ -43,25 +43,25 @@ void EvAPI::init() {
 
     for (auto& ev : this->r_ev_manager) {
         auto& session_info = this->info.emplace_back(std::make_unique<EvSessionInfo>());
-        std::string ev_base = this->api_base + ev->module_id;
+        const std::string ev_base = this->api_base + ev->module_id;
         ev_connectors.push_back(ev->module_id);
 
         // API variables
-        std::string var_base = ev_base + "/var/";
+        const std::string var_base = ev_base + "/var/";
 
-        std::string var_ev_info = var_base + "ev_info";
+        const std::string var_ev_info = var_base + "ev_info";
         ev->subscribe_ev_info([this, &ev, var_ev_info](types::evse_manager::EVInfo ev_info) {
             json ev_info_json = ev_info;
             this->mqtt.publish(var_ev_info, ev_info_json.dump());
         });
 
-        std::string var_session_info = var_base + "session_info";
+        const std::string var_session_info = var_base + "session_info";
         ev->subscribe_bsp_event([this, var_session_info, &session_info](const auto& bsp_event) {
             session_info->update_event(bsp_event.event);
             this->mqtt.publish(var_session_info, *session_info);
         });
 
-        std::string var_datetime = var_base + "datetime";
+        const std::string var_datetime = var_base + "datetime";
         this->api_threads.push_back(std::thread([this, var_datetime, var_session_info, &session_info]() {
             auto next_tick = std::chrono::steady_clock::now();
             while (this->running) {
@@ -75,13 +75,13 @@ void EvAPI::init() {
         }));
 
         // API commands
-        std::string cmd_base = ev_base + "/cmd/";
+        const std::string cmd_base = ev_base + "/cmd/";
     }
 
     this->api_threads.push_back(std::thread([this, var_ev_connectors, ev_connectors]() {
         auto next_tick = std::chrono::steady_clock::now();
         while (this->running) {
-            json ev_connectors_array = ev_connectors;
+            const json ev_connectors_array = ev_connectors;
             this->mqtt.publish(var_ev_connectors, ev_connectors_array.dump());
 
             next_tick += NOTIFICATION_PERIOD;
@@ -93,17 +93,15 @@ void EvAPI::init() {
 void EvAPI::ready() {
     invoke_ready(*p_main);
 
-    std::string var_active_errors = this->api_base + "errors/var/active_errors";
+    const std::string var_active_errors = this->api_base + "errors/var/active_errors";
     this->api_threads.push_back(std::thread([this, var_active_errors]() {
         auto next_tick = std::chrono::steady_clock::now();
         while (this->running) {
-            std::string datetime_str = Everest::Date::to_rfc3339(date::utc_clock::now());
-
             if (not r_error_history.empty()) {
                 // request active errors
                 types::error_history::FilterArguments filter;
                 filter.state_filter = types::error_history::State::Active;
-                auto active_errors = r_error_history.at(0)->call_get_errors(filter);
+                const auto active_errors = r_error_history.at(0)->call_get_errors(filter);
                 json errors_json = json(active_errors);
 
                 // publish
